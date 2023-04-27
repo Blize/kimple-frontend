3a7e09af48a2ed92e8da0b1ae0688f7d39373989
simylein <67859356+simylein@users.noreply.github.com>
2023-04-27 14:17:11 +0200

    chore(setup): initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactElement, ReactNode } from 'react';
+import './globals.css';
+
+export const metadata = {
+	title: 'Kimple',
+	description: 'Take Notes with Kimple',
+};
+
+type Props = {
+	children: ReactNode;
+};
+
+export default function RootLayout({ children }: Props): ReactElement {
+	return (
+		<html lang={'en'}>
+			<body>{children}</body>
+		</html>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+export default function Home(): ReactElement {
+	return (
+		<main>
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		</main>
+	);
+}
