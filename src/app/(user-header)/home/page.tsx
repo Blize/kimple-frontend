99a713d4e00215e55affd4668f319923e3613300
Slowlydev <slowly.developers@gmail.com>
2023-06-01 23:10:40 +0200

    feat(home-page): add api calls to get notes aswell as create cards

--- src/app/(user-header)/home/page.tsx
+++ src/app/(user-header)/home/page.tsx
@@ -1,13 +1,44 @@
 import { cookies } from 'next/headers';
 import { ReactElement } from 'react';
 
-import { getCurrentUser } from '@/services/auth.service';
+import { Note } from '@/types/note.type';
+
+import { getNotes } from '@/services/notes.service';
+
+import CreateCard from '@/components/CreateCard/CreateCard';
+import Line from '@/components/Line/Line';
+
+import styles from './home.module.css';
 
 const HomePage = async (): Promise<ReactElement> => {
-	const tokenCookie = cookies().get('token');
-	const user = await getCurrentUser(tokenCookie?.value ?? '');
+	const cookieToken = cookies().get('token')?.value ?? '';
 
-	return <div>user: {JSON.stringify(user) ?? ''}</div>;
+	const notes: Note[] | null = (await getNotes(cookieToken)) ?? null;
+
+	return (
+		<div>
+			<p>Recently Viewed</p>
+
+			<div>{/* cards */}</div>
+
+			<Line />
+
+			<p>Notes</p>
+
+			<div className={styles.cardContainer}>
+				{notes && notes.map((note) => <p key={`notes.${note.id}`}>{note.title}</p>)} <CreateCard label="Add Note" />
+			</div>
+
+			<Line />
+
+			<p>Todo Lists</p>
+
+			<div className={styles.cardContainer}>
+				{/* cards */}
+				<CreateCard label="Add Todo List" />
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
