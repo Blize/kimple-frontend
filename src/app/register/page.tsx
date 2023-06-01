1c695df9b0a2179f96398fee1e6cf0a3488488e7
Slowlydev <slowly.developers@gmail.com>
2023-06-01 20:54:49 +0200

    refactor(register): implement auth and change styles to match design

---
+++ src/app/register/page.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { env } from '@/env.mjs';
+import { setCookie } from 'cookies-next';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { ReactElement, useState } from 'react';
+
+import { login, register } from '@/services/auth.service';
+
+import { useNotifications } from '@/providers/NotificationProvider';
+
+import Button from '@/components/Button/Button';
+import Input from '@/components/Input/Input';
+
+import styles from './page.module.css';
+
+const RegisterPage = (): ReactElement => {
+	const { addSuccess, addError } = useNotifications();
+	const [username, setUsername] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	const router = useRouter();
+
+	const handleRegister = async (username: string, password: string): Promise<void> => {
+		console.log({ password, username });
+
+		if (!username || !password) return;
+
+		try {
+			await register({ username, password });
+			const { token } = await login({ username, password });
+			setCookie('token', token, {
+				sameSite: true,
+				expires: new Date(Date.now() + env.NEXT_PUBLIC_COOKIE_EXPIRY * 1000 * 14),
+			});
+
+			addSuccess('successfully registered');
+
+			router.push('/home');
+		} catch (err) {
+			addError('registering failed', err);
+		}
+	};
+
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.container}>
+				<h1>Welcome please register</h1>
+
+				<div className={styles.fields}>
+					<Input
+						placeholder={'username'}
+						value={username}
+						autoFocus={true}
+						onChange={(e) => setUsername(e.target.value)}
+						className={styles.input}
+					/>
+
+					<Input
+						type={'password'}
+						placeholder={'password'}
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						className={styles.input}
+					/>
+				</div>
+
+				<Button onClick={() => handleRegister(username, password)}>Register</Button>
+
+				<Link href={'/login'}>or login here</Link>
+			</div>
+		</div>
+	);
+};
+
+export default RegisterPage;
--- src/env.mjs
+++ src/env.mjs
@@ -15,6 +15,7 @@ const server = z.object({
 const client = z.object(
 	/** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ ({
 		NEXT_PUBLIC_BACKEND_URL: z.string().url(),
+		NEXT_PUBLIC_COOKIE_EXPIRY: z.preprocess((val) => Number(val), z.number()),
 	}),
 );
 
@@ -27,6 +28,7 @@ const client = z.object(
 const processEnv = {
 	NODE_ENV: process.env.NODE_ENV,
 	NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+	NEXT_PUBLIC_COOKIE_EXPIRY: process.env.NEXT_PUBLIC_COOKIE_EXPIRY,
 };
 
 // Don't touch the part below
