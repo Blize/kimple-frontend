5aed55cb9aebe899a55eddcd90c215c9e74b225d
Slowlydev <slowly.developers@gmail.com>
2023-06-01 22:16:58 +0200

    feat(nav): add navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,26 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { ReactElement } from 'react';
 
+import Button from '../Button/Button';
 import styles from './Navigation.module.css';
 
 const Navigation = (): ReactElement => {
-	return <div></div>;
+	const pathname = usePathname();
+
+	return (
+		<div className={styles.container}>
+			<Link href={'/home'}>
+				<Button className={pathname === '/home' ? styles.active : ''}>Home</Button>
+			</Link>
+
+			<Link href={'/editor'}>
+				<Button className={pathname === '/editor' ? styles.active : ''}>Editor</Button>
+			</Link>
+		</div>
+	);
 };
 
 export default Navigation;
