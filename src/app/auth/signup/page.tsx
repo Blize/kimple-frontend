617540ca6b8f3917994203b8b6bb9dcf8ef59d54
simylein <67859356+simylein@users.noreply.github.com>
2023-06-01 13:24:28 +0200

    refactor(signup): adjust to acceptance criteria

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,9 +4,8 @@ import Input from '../../../components/Input/Input';
 import styles from './page.module.css';
 
 const SignUpPage = (): ReactElement => {
-	const [userName, setUserName] = useState<string>('');
-	const [email, setEmail] = useState<string>('');
-	const [passWord, setPassWord] = useState<string>('');
+	const [username, setUsername] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
 
 	return (
 		<div className={styles.container}>
@@ -15,15 +14,13 @@ const SignUpPage = (): ReactElement => {
 				<label htmlFor="username">Username</label>
 				<Input
 					type={'text'}
-					value={userName}
+					value={username}
 					name={'username'}
 					autoFocus={true}
-					onChange={(v) => setUserName(v.target.value)}
+					onChange={(v) => setUsername(v.target.value)}
 				/>
-				<label htmlFor="email">Email</label>
-				<Input type={'email'} value={email} name={'email'} onChange={(v) => setEmail(v.target.value)} />
 				<label htmlFor="password">Password</label>
-				<Input type={'password'} value={passWord} name={'password'} onChange={(v) => setPassWord(v.target.value)} />
+				<Input type={'password'} value={password} name={'password'} onChange={(v) => setPassword(v.target.value)} />
 			</div>
 		</div>
 	);
