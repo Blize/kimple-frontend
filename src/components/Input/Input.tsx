70b497f1f35957b9d226bba2c59910e8beee5681
Slowlydev <slowly.developers@gmail.com>
2023-06-01 20:47:44 +0200

    refactor(input-component): make some style and usage improvements

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,23 @@
+import clsx from 'clsx';
 import { ChangeEvent, ReactElement } from 'react';
 
 import styles from './Input.module.css';
 
 type Props = {
-	type: 'text' | 'email' | 'password';
 	value: string;
+	type?: 'text' | 'email' | 'password';
 	name?: string;
 	placeholder?: string;
 	autoFocus?: boolean;
-	onChange?: (v: ChangeEvent<HTMLInputElement>) => void;
+	onChange: (v: ChangeEvent<HTMLInputElement>) => void;
 	onBlur?: (v: ChangeEvent<HTMLInputElement>) => void;
 	className?: string;
 };
 
-const Input = ({ type, value, name, placeholder, autoFocus, onChange, onBlur }: Props): ReactElement => {
+const Input = ({ type, value, name, placeholder, autoFocus, className, onChange, onBlur }: Props): ReactElement => {
 	return (
 		<input
-			className={styles.input}
+			className={clsx(className, styles.input)}
 			name={name}
 			type={type}
 			value={value}
