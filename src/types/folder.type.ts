dc82236901714f4d221b022341407b39574d6d68
Slowlydev <slowly.developers@gmail.com>
2023-06-06 21:50:05 +0200

    feat(folder-type): create folder type

--- src/types/folder.type.ts
+++ src/types/folder.type.ts
@@ -1,8 +1,22 @@
 import { Base } from './base.type';
+import { Note } from './note.type';
+import { Todo } from './todo.type';
 
 export type Folder = Base & {
 	id: string;
 	title: string;
 	userId: string;
 	parentFolderId: string | null;
+
+	subFolders?: (Folder | Todo | Note)[];
+};
+
+export type CreateFolder = {
+	title: string;
+	parentFolderId?: string;
+};
+
+export type UpdateFolder = {
+	title?: string;
+	parentFolderId?: string;
 };
