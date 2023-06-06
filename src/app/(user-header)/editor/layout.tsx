2d1d4b1f5f8754f575fdab7c6e1171c646305b2a
Slowlydev <slowly.developers@gmail.com>
2023-06-06 21:54:42 +0200

    feat(editor): add explorer to layout and fix css

--- src/app/(user-header)/editor/layout.tsx
+++ src/app/(user-header)/editor/layout.tsx
@@ -1,4 +1,11 @@
+import { cookies } from 'next/headers';
 import { ReactElement, type ReactNode } from 'react';
+
+import { getFolders } from '@/services/folder.service';
+
+import { ExplorerProvider } from '@/providers/ExplorerProvider';
+
+import Explorer from '@/components/Explorer/Explorer';
 
 import styles from './layout.module.css';
 
@@ -6,10 +13,20 @@ type Props = {
 	children: ReactNode;
 };
 
-export default function Layout({ children }: Props): ReactElement {
+export default async function Layout({ children }: Props): Promise<ReactElement> {
+	const tokenCookie = cookies().get('token')?.value ?? '';
+
+	const folders = await getFolders(tokenCookie, true, true);
+
 	return (
 		<div className={styles.container}>
-			<div className={styles.folders}>folders</div>
+			<div className={styles.folders}>
+				{/* TODO search */}
+
+				<ExplorerProvider>
+					<Explorer tree={folders} />
+				</ExplorerProvider>
+			</div>
 
 			<div className={styles.notes}>{children}</div>
 		</div>
