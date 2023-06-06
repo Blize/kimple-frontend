e19953cdefb1491c126963ff5d793b27e40bebbc
Slowlydev <slowly.developers@gmail.com>
2023-06-06 21:51:39 +0200

    feat(explorer-todo): added todo item for explorer

---
+++ src/components/ExplorerTodo/ExplorerTodo.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+import { Todo } from '@/types/todo.type';
+
+import todoIcon from '@/assets/todo.svg';
+
+import styles from './ExplorerTodo.module.css';
+
+type Props = {
+	todo: Todo;
+};
+
+const ExplorerTodo = ({ todo }: Props): ReactElement => {
+	return (
+		<Link href={`/editor/${todo.id}`}>
+			<div className={styles.todo}>
+				<Image src={todoIcon} width={20} height={20} alt="todo icon" />
+				<p>{todo.title ?? todo.todoItems[0].name ?? 'Empty Todo'}</p>
+			</div>
+		</Link>
+	);
+};
+
+export default ExplorerTodo;
